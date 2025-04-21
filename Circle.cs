8a8a85b90bc8a73adadcdd5adbe846c468eb0c80
Christopher Williams <williamstophef@gmail.com>
2025-04-21 15:20:07 -0700

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3._2._3
+{
+    internal struct Circle
+    {
+        public double radius;
+        public double area;
+
+        public Circle(double r)
+        {
+            radius = r;
+            area = 3.1415 * r * r;
+        }
+
+        public static double operator +(Circle a, Circle b)
+        {
+            return a.area + b.area;
+        }
+
+        public static double operator -(Circle a, Circle b)
+        {
+            return a.area - b.area;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Assignment3._2._3;
+
+Console.WriteLine("Enter the radius of circle 1. ");
+double radius1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the radius of circle 2. ");
+double radius2 = Convert.ToDouble(Console.ReadLine());
+
+Circle circle1 = new Circle(radius1);
+Circle circle2 = new Circle(radius2);
+
+double sum = circle1 + circle2;
+double difference = circle1 - circle2;
+
+Console.WriteLine($"Area of circle 1 is {circle1.area}");
+Console.WriteLine($"Area of circle 2 is {circle2.area}");
+Console.WriteLine($"Sum of the area of the 2 circles is {sum}");
+Console.WriteLine($"Difference of the area of the 2 circles is {difference}");
